18f7db0e1a08186d8576d795e3188e28b018c46f
Benedikt Spies <benedikt.spies@gmail.com>
2026-05-05 14:28:54 +0200

    ebpf example support arm

--- examples/ebpf/flake.nix
+++ examples/ebpf/flake.nix
@@ -1,64 +1,74 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixnet.url = "github:birneee/nixnet";
     starlink.url = "github:birneee/simple-starlink-ebpf";
   };
   outputs =
-    inputs@{ nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      nixnet = inputs.nixnet.legacyPackages.${system};
-      starlink = inputs.starlink.packages.${system}.default;
-    in
-    {
-      packages.${system}.default = nixnet.mkTestbed {
-        namespacePackages = with pkgs; [ iperf3 coreutils ];
-        namespaces = {
-          client = {
-            networking.interfaces.veth0.ipv4.addresses = [
-              {
-                address = "10.0.0.1";
-                prefixLength = 24;
-              }
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      perSystem =
+        { pkgs, inputs', ... }:
+        let
+          nixnet = inputs'.nixnet.legacyPackages;
+          starlink = inputs'.starlink.packages.default;
+        in
+        {
+          packages.default = nixnet.mkTestbed {
+            namespacePackages = with pkgs; [
+              iperf3
+              coreutils
             ];
-            postSetup = "ip link set dev veth0 xdp obj ${starlink}/starlink.o sec xdp";
-            scripts = [
+            namespaces = {
+              client = {
+                networking.interfaces.veth0.ipv4.addresses = [
+                  {
+                    address = "10.0.0.1";
+                    prefixLength = 24;
+                  }
+                ];
+                postSetup = "ip link set dev veth0 xdp obj ${starlink}/starlink.o sec xdp";
+                scripts = [
+                  {
+                    exec = "sleep 0.1; iperf3 -c 10.0.0.2 -t 30 --forceflush | tee stdout";
+                    await = true;
+                  }
+                ];
+              };
+              server = {
+                networking.interfaces.veth0.ipv4.addresses = [
+                  {
+                    address = "10.0.0.2";
+                    prefixLength = 24;
+                  }
+                ];
+                postSetup = "ip link set dev veth0 xdp obj ${starlink}/starlink.o sec xdp";
+                scripts = [
+                  {
+                    exec = "iperf3 -s";
+                  }
+                ];
+              };
+            };
+            veths = [
               {
-                exec = "sleep 0.1; iperf3 -c 10.0.0.2 -t 30 --forceflush";
-                await = true;
-              }
-            ];
-          };
-          server = {
-            networking.interfaces.veth0.ipv4.addresses = [
-              {
-                address = "10.0.0.2";
-                prefixLength = 24;
-              }
-            ];
-            postSetup = "ip link set dev veth0 xdp obj ${starlink}/starlink.o sec xdp";
-            scripts = [
-              {
-                exec = "iperf3 -s";
+                netem.delayMs = 40;
+                a = {
+                  ns = "client";
+                  iface = "veth0";
+                };
+                b = {
+                  ns = "server";
+                  iface = "veth0";
+                };
               }
             ];
           };
         };
-        veths = [
-          {
-            netem.delayMs = 40;
-            a = {
-              ns = "client";
-              iface = "veth0";
-            };
-            b = {
-              ns = "server";
-              iface = "veth0";
-            };
-          }
-        ];
-      };
     };
 }
